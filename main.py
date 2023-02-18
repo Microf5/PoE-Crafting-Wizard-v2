d9d785bdc7b9d102999f2edd37819b039b046a9e
Microf5 <thomaswei3456@gmail.com>
2023-02-17 21:43:26 -0600

    prayge
    grab everything we need for alt spamming
    a little questionable alt location tho - maybe train image AI for it or just find the common ratio/location for all monitor sizes (tbh this might actually be the best way LOL)
    
    left todo - the actual spamming part.

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
 import pyautogui as pyag
 import time
 import cv2, numpy
+
+
 def print_hi(name):
+    screenWidth, screenHeight = pyautogui.size()
+    time.sleep(1)
+    altLocation = pyag.locateCenterOnScreen('alteration.png', region=(0, 0, screenWidth, screenHeight),
+                                                 confidence=(.65))
+    # prob need to find a better way to locate this, it like - only works depending on how many alts u have
+    print(altLocation)
+    pyag.moveTo(altLocation)
+    craftLocation = pyag.locateCenterOnScreen('wherecraft.png', region=(0, 0, screenWidth, screenHeight),
+                                                   confidence=(.9))
+    print(craftLocation)
+    pyag.moveTo(craftLocation)
+    searchLocation = pyg.locateCenterOnScreen('searchbox.png', region=(0, 0, screenWidth, screenHeight),
+                                                    confidence=(.9))
+    print(searchLocation)
+    pyag.moveTo(searchLocation)
 
 
-   screenWidth, screenHeight = pyag.size()
-   time.sleep(2)
-   r = pyag.locateOnScreen('kekw.png',region=(0, 0, screenWidth, screenHeight),confidence = (0.8),grayscale = True)
-   print(r)
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   print_hi('PyCharm')
+    print_hi('PyCharm')
+
+# TODO:
+# MAKE STUFF WORK, THEN ADD ALT REGAL SPAM
+# MAY NEED TO GET THE GRAYSCALED(ones that dont fit search) IMAGES OF SOME ITEMS
+# STUFF EVERYTHING INTO A MAP/DICTIONARY?
+# IMPLEMENT FAILSAFES -> NOT ON RIGHT STASHTAB OR RUN OUT OF A MATERIAL OR SOMETHING WRONG
+
+
+# ALT REGAL SPAM STUFF
+
+
+# regalLocation = pyautogui.locateCenterOnScreen('regal.png', region=(0, 0, screenWidth, screenHeight),confidence=(.6))
+# print(regalLocation)
+# pyautogui.moveTo(regalLocation)
+# scourLocation = pyautogui.locateCenterOnScreen('scour.png', region=(0, 0, screenWidth, screenHeight),confidence=(.6))
+# print(scourLocation)
+# pyautogui.moveTo(scourLocation)
+# transmuteLocation = pyautogui.locateCenterOnScreen('transmute.png', region=(0, 0, screenWidth, screenHeight),confidence=(.6))
+# print(transmuteLocation)
+# pyautogui.moveTo(transmuteLocation)
+
